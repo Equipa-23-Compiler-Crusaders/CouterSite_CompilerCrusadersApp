8187d4a51246264ad928f07bc17234147b1e2b89
Rui Martins <rui_miguel97@hotmail.com>
2023-06-03 18:39:23 +0100

    buttons

--- compilercrusadersteste/Form1.Designer.cs
+++ compilercrusadersteste/Form1.Designer.cs
@@ -36,7 +36,6 @@
             this.btnCerrar = new System.Windows.Forms.PictureBox();
             this.MenuVertical = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
-            this.google = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panelContentor = new System.Windows.Forms.Panel();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
@@ -127,7 +126,6 @@
             // 
             this.MenuVertical.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.MenuVertical.Controls.Add(this.button1);
-            this.MenuVertical.Controls.Add(this.google);
             this.MenuVertical.Controls.Add(this.pictureBox1);
             this.MenuVertical.Dock = System.Windows.Forms.DockStyle.Left;
             this.MenuVertical.Location = new System.Drawing.Point(0, 31);
@@ -142,37 +140,22 @@
             this.button1.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(38, 294);
+            this.button1.Location = new System.Drawing.Point(2, 279);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(138, 37);
+            this.button1.Size = new System.Drawing.Size(202, 58);
             this.button1.TabIndex = 3;
             this.button1.Text = "Sair";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
-            // google
-            // 
-            this.google.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.google.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.google.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.google.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.google.Location = new System.Drawing.Point(38, 217);
-            this.google.Margin = new System.Windows.Forms.Padding(2);
-            this.google.Name = "google";
-            this.google.Size = new System.Drawing.Size(138, 46);
-            this.google.TabIndex = 1;
-            this.google.Text = "Pesquisar no Google";
-            this.google.UseVisualStyleBackColor = false;
-            this.google.Click += new System.EventHandler(this.google_Click);
-            // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(2, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(2, 89);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(204, 128);
+            this.pictureBox1.Size = new System.Drawing.Size(200, 157);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -334,7 +317,6 @@
         private System.Windows.Forms.PictureBox btnMinimizar;
         private System.Windows.Forms.Panel panelContentor;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button google;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label TituloLocalizar;
