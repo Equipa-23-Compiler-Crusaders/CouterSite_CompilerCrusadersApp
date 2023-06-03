e59a4a9e3017d2e1ac745bffeaa99a52398e99ea
RicardoCosta-UAB <2100559@estudante.uab.pt>
2023-06-03 17:50:57 +0100

    Incorporação do interface

--- compilercrusadersteste/Controller.cs
+++ compilercrusadersteste/Controller.cs
@@ -8,7 +8,7 @@ using System.Windows.Forms;
 
 namespace compilercrusadersteste
 {
-    class Controller
+    class Controller : IControladorPesquisa
     {
 
 
---
+++ compilercrusadersteste/IControladorPesquisa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace compilercrusadersteste
+{
+    public interface IControladorPesquisa
+    {
+        void IniciarPrograma();
+        void CliqueEmPesquisar(object origem, EventArgs e);
+        void GerarResultados(object origem, EventArgs e);
+        void Erros(object origem, EventArgs e);
+    }
+}
